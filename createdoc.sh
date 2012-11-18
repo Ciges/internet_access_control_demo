262f57a62c0ff5552040e237612a5ab050ce8e09
José Manuel Ciges Regueiro <jmanuel@ciges.net>
2012-11-18 19:32:56 +0100

    Updating source code documentation (1 of 2)

--- createdoc.sh
+++ createdoc.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-alias php="$PHP_PEAR_PHP_BIN -c $PHP_CONFIG"
-alias phpdoc="php /users2/mnd01/libext/php/pear/bin/phpdoc"
 phpdoc --sourcecode on -d classes -o HTML:Smarty:HandS -t doc/
 
